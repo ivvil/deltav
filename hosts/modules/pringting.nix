4dc87a93d25cac9c8abdd241915879aef4d2b090
Iván Villagrasa Pérez <56278092+ivvil@users.noreply.github.com>
2024-09-22 17:12:21 +0200

    Add printig, steam, video and virtualization nixos config

---
+++ hosts/modules/pringting.nix
@@ -0,0 +1,20 @@
+{
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true;
+    openFirewall = true;
+    publish = {
+      enable = true;
+      userServices = true;
+    };
+  };
+  services.printing = {
+    enable = true;
+    # drivers = [ pkgs.hplip ];
+    listenAddresses = ["*:631"];
+    allowFrom = ["all"];
+    browsing = true;
+    defaultShared = true;
+    openFirewall = true;
+  };
+}
