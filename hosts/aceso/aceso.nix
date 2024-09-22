b71cb1b21e3030d24e7861822fd9afa597c1208f
Iván Villagrasa Pérez <56278092+ivvil@users.noreply.github.com>
2024-09-22 23:24:15 +0200

    Add modules and home

--- hosts/aceso/aceso.nix
+++ hosts/aceso/aceso.nix
@@ -1,5 +1,28 @@
 {config, ...}: {
-  imports = [./variables.nix];
+  imports = [
+    ../modules/audio.nix
+    ../modules/boot.nix
+    ../modules/flatpak.nix
+    ../modules/fonts.nix
+    ../modules/fs.nix
+    ../modules/gnome.nix
+    ../modules/graphics.nix
+    ../modules/home-manager.nix
+    ../modules/locale.nix
+    ../modules/networking.nix
+    ../modules/nix.nix
+    ../modules/nvidia.nix
+    ../modules/openssh.nix
+    ../modules/pringting.nix
+    ../modules/steam.nix
+    ../modules/sudo.nix
+    ../modules/swap.nix
+    ../modules/user.nix
+    ../modules/variables-config.nix
+    ../modules/video.nix
+    ../modules/virtualization.nix
+    ../modules/wayland.nix
+  ];
 
   home-manager.users."$(config.var.username)" = import ./home.nix;
 
