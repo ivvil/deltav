0ed05ab61b587d2e5dec5a8c59fa4d75bc71f5e7
Iván Villagrasa Pérez <56278092+ivvil@users.noreply.github.com>
2024-09-23 22:11:20 +0200

    add syncthing config

---
+++ home/programs/syncthing.nix
@@ -0,0 +1,28 @@
+{config, ...}: {
+  services.syncthing = {
+    enable = true;
+    user = "ivvil";
+    settings.gui.password = "";
+    settings.gui.user = "ivvil";
+    configDir = "/home/ivvil/.config/syncthing";
+    overrideDevices = true; # overrides any devices added or deleted through the WebUI
+    overrideFolders = true; # overrides any folders added or deleted through the WebUI
+    settings.devices = {
+      "abas" = {id = config.sops.secrets."syncthing/devices/";};
+      "aceso" = {id = "";};
+    };
+    settings.folders = {
+      "Música" = {
+        id = "";
+        path = "/home/ivvil/Música";
+        devices = ["abas" "aceso"];
+      };
+
+      "KeePass" = {
+        id = "";
+        path = "/home/ivvil/Documentos/KeePass";
+        devices = ["abas" "aceso"];
+      };
+    };
+  };
+}
