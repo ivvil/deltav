53dadc413c7d308e3337d415644dcf07ed9106af
Iván Villagrasa Pérez <56278092+ivvil@users.noreply.github.com>
2024-10-23 09:15:33 +0200

    Add vterm config

--- home/programs/sh.nix
+++ home/programs/sh.nix
@@ -1,13 +1,9 @@
-{
-  pkgs,
-  config,
-  ...
-}: let
+{pkgs, ...}: let
   shellAliases = {
     "db" = "distrobox";
     "n" = "nano";
     "em" = "emacs";
-    "sudo " = "sudo";
+    # "sudo " = "sudo";
     "sv" = "systemctl";
     "del" = "gio trash";
     "c" = "clear";
@@ -26,7 +22,35 @@ in {
       autosuggestion.enable = true;
       syntaxHighlighting.enable = true;
       initExtra = ''
-        SHELL=${pkgs.zsh}/bin/zsh '';
+        SHELL=${pkgs.zsh}/bin/zsh
+        vterm_printf() {
+            if [ -n "$TMUX" ] && ([ ''\"''${TERM%%-*}" = "tmux" ] || [ ''\"''${TERM%%-*}" = "screen" ]); then
+                # Tell tmux to pass the escape sequences through
+                printf "\ePtmux;\e\e]%s\007\e\\" "$1"
+            elif [ ''\"''${TERM%%-*}" = "screen" ]; then
+                # GNU screen (screen, screen-256color, screen-256color-bce)
+                printf "\eP\e]%s\007\e\\" "$1"
+            else
+                printf "\e]%s\e\\" "$1"
+            fi
+        }
+
+        vterm_prompt_end() {
+        vterm_printf "51;A$(whoami)@$(hostname):$(pwd)"
+        }
+        setopt PROMPT_SUBST
+        PROMPT=$PROMPT'%{$(vterm_prompt_end)%}'
+
+       vterm_cmd() {
+           local vterm_elisp
+           vterm_elisp=""
+           while [ $# -gt 0 ]; do
+               vterm_elisp="$vterm_elisp""$(printf '"%s" ' "$(printf "%s" "$1" | sed -e 's|\\|\\\\|g' -e 's|"|\\"|g')")"
+               shift
+           done
+           vterm_printf "51;E$vterm_elisp"
+       }
+      '';
     };
 
     bash = {
